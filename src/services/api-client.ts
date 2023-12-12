e12b20c37aa667265813451a288f26cf8097c6ba
809902 <amanwankar07@gmail.com>
2023-12-12 22:08:14 +0530

    minor changes and env var for rawg api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '62d3be09da2c4dd1a72a6d06342eb00a',
+    key: import.meta.env.VITE_API_KEY,
   },
 });
 
