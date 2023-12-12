c9cd7a677e431df95c53924bf5c61ccb7a801eff
809902 <amanwankar07@gmail.com>
2023-12-12 07:28:21 +0530

    Build game trailer component

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,8 +1,9 @@
 import { Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
-import useGame from '../hooks/useGame';
 import { GameAttributes } from '../components/GameAttributes';
+import GameTrailer from '../components/GameTrailer';
+import useGame from '../hooks/useGame';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -16,7 +17,8 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game}></GameAttributes>
+      <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
